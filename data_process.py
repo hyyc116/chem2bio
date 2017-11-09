6b0a23b1c46b9dfe4374fed2ff29cdb23ec332ff
hyyc116 <hyyc116@gmail.com>
2017-11-08 20:25:13 -0500

    add

---
+++ data_process.py
@@ -0,0 +1,48 @@
+#coding:utf-8
+import json
+
+def generate_edges(path):
+    
+    entity_list = set()
+    relation_list = set()
+
+    relation_id = {}
+    entity_id = {}
+    for line in open(path):
+        line = line.strip()
+        e1,r,e2 = line.split('\t')
+
+        if e1 not in entity_list:
+            entity_list.add(e1)
+            entity_id[e1] = len(entity_list)
+
+
+        if e2 not in entity_list:
+            entity_list.add(e2)
+            entity_id[e2] = len(entity_list)
+
+        if r not in relation_list:
+            relation_list.add(r)
+            relation_id[r] = len(relation_list)
+
+    open('data/entity_id.json','w').write(json.dumps(entity_id))
+    open('data/relation_id.json','w').write(json.dumps(relation_id))
+
+    lines = []
+    for line in open(path):
+        line = line.strip()
+        e1,r,e2 = line.split('\t')
+        line = '{:} {:}'.format(entity_id[e1],entity_id[e2])
+
+        lines.append(line)
+
+    open('data/trans_data.txt','w').write('\n'.join(lines))
+
+
+if __name__ == '__main__':
+    generate_edges(sys.argv[1])
+
+
+
+
+
